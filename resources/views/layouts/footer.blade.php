c70c4634f93be470b169a20c200ff198253afef4
Mang <fathinalthafunnisa@gmail.com>
2023-07-18 12:03:11 +0700

    Toifur Rahman

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,53 @@
+<div class="footer" style="font-family: 'Dancing Script', cursive;
+font-family: 'Inter', sans-serif;">
+    <div class="d-flex justify-content-between gap-5 pt-4 mt-5">
+        <div class="title-left w-25" style="margin-left: 10px;">
+            <div class="header-title fs-4 mb-2 font-weight-bolder">
+                TOKO SEPATU
+            </div>
+            <p>Sepatu berkualitas dengan harga bersahabat ya disini tempatnya.
+                sudah mempunyai lisensi jadi teman teman tidak perlu takut sepatu yang
+                teman teman beli adalah sepatu yang palsu dan memiliki kualitas yang buruk.
+            </p>
+        </div>
+        <div class="title-middle w-25">
+            <div class="header-title fs-4 mb-2 font-weight-bolder">
+                Tentang Kami
+            </div>
+            <ul class="list-unstyled">
+                <li>
+                    <p>
+                        Kami juga ada offline store yang bisa teman teman kunjungi, alamatnya di Jl.kajsasjfajsfalksf
+                    </p>
+                </li>
+                <li>
+                    <p>join reseller hubungi email berikut</p>
+                </li>
+                <li>
+                    tokosepatu@gmail.com
+                </li>
+            </ul>
+        </div>
+        <div class="title-sosmed w-30">
+            <div class="header-title fs-4 mb-2 font-weight-bolder" style="margin-right: 10px;">
+                Sosial Media
+            </div>
+            <div class="sosmed d-flex gap-3">
+                <i class="bi bi-instagram"></i>
+                <i class="bi bi-facebook"></i>
+                <i class="bi bi-whatsapp"></i>
+            </div>
+        </div>
+    </div>
+    <div class="text-center p-lg-4 mt-3">
+        Copy right @TokoSepatu</i>
+    </div>
+</div>
+<style>
+    .footer{
+    position: static;
+    bottom: 0;
+    background-color: #A0C49D;
+    color: white;
+    }
+</style>
