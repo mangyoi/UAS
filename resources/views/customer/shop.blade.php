d6d5822504e1b2a7d790c00a3856cd73bf1425f2
bilqiisaprilliaa <bilqiisar21@gmail.com>
2023-07-26 14:07:16 +0700

    bilqis update shop

---
+++ resources/views/customer/shop.blade.php
@@ -0,0 +1,120 @@
+@extends('welcome')
+
+@section('content')
+    <div class="row mt-4 justify-content-center">
+        <div class="d-flex flex-wrap gap-4 mb-4 " >
+            <div class="card" style="width:220px;">
+                <div class="card-header m-auto" style="border-radius:5px;">
+                    <img src="{{Vite::asset('resources/images/70s.jpg')}}" alt="sepatu" style="width: 100%;">
+                </div>
+                <div class="card-body">
+                    <p class="m-0 text-justify"> converse 70s </p>
+                </div>
+                <div class="card-footer d-flex flex-row justify-content-between align-items-center">
+                    <p class="m-0" style="font-size: 16px; font-weight:600;">Rp. 560.000</p>
+                    <button class="btn btn-outline-primary" style="font-size:24px">
+                        <i class="bi bi-bag-check-fill"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card" style="width:220px;">
+                <div class="card-header m-auto" style="border-radius:5px;">
+                    <img src="{{Vite::asset('resources/images/astroboy.jpg')}}" alt="sepatu" style="width: 100%;">
+                </div>
+                <div class="card-body">
+                    <p class="m-0 text-justify"> converse 70s </p>
+                </div>
+                <div class="card-footer d-flex flex-row justify-content-between align-items-center">
+                    <p class="m-0" style="font-size: 16px; font-weight:600;">Rp. 560.000</p>
+                    <button class="btn btn-outline-primary" style="font-size:24px">
+                        <i class="bi bi-bag-check-fill"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card" style="width:220px;">
+                <div class="card-header m-auto" style="border-radius:5px;">
+                    <img src="{{Vite::asset('resources/images/checkboard.jpg')}}" alt="sepatu" style="width: 100%;">
+                </div>
+                <div class="card-body">
+                    <p class="m-0 text-justify"> converse 70s </p>
+                </div>
+                <div class="card-footer d-flex flex-row justify-content-between align-items-center">
+                    <p class="m-0" style="font-size: 16px; font-weight:600;">Rp. 560.000</p>
+                    <button class="btn btn-outline-primary" style="font-size:24px">
+                        <i class="bi bi-bag-check-fill"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card" style="width:220px;">
+                <div class="card-header m-auto" style="border-radius:5px;">
+                    <img src="{{Vite::asset('resources/images/sepatugunung.jpeg')}}" alt="sepatu" style="width: 100%;">
+                </div>
+                <div class="card-body">
+                    <p class="m-0 text-justify"> converse 70s </p>
+                </div>
+                <div class="card-footer d-flex flex-row justify-content-between align-items-center">
+                    <p class="m-0" style="font-size: 16px; font-weight:600;">Rp. 560.000</p>
+                    <button class="btn btn-outline-primary" style="font-size:24px">
+                        <i class="bi bi-bag-check-fill"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card" style="width:220px;">
+                <div class="card-header m-auto" style="border-radius:5px;">
+                    <img src="{{Vite::asset('resources/images/70s.jpg')}}" alt="sepatu" style="width: 100%;">
+                </div>
+                <div class="card-body">
+                    <p class="m-0 text-justify"> converse 70s </p>
+                </div>
+                <div class="card-footer d-flex flex-row justify-content-between align-items-center">
+                    <p class="m-0" style="font-size: 16px; font-weight:600;">Rp. 560.000</p>
+                    <button class="btn btn-outline-primary" style="font-size:24px">
+                        <i class="bi bi-bag-check-fill"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card" style="width:220px;">
+                <div class="card-header m-auto" style="border-radius:5px;">
+                    <img src="{{Vite::asset('resources/images/70s.jpg')}}" alt="sepatu" style="width: 100%;">
+                </div>
+                <div class="card-body">
+                    <p class="m-0 text-justify"> converse 70s </p>
+                </div>
+                <div class="card-footer d-flex flex-row justify-content-between align-items-center">
+                    <p class="m-0" style="font-size: 16px; font-weight:600;">Rp. 560.000</p>
+                    <button class="btn btn-outline-primary" style="font-size:24px">
+                        <i class="bi bi-bag-check-fill"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card" style="width:220px;">
+                <div class="card-header m-auto" style="border-radius:5px;">
+                    <img src="{{Vite::asset('resources/images/70s.jpg')}}" alt="sepatu" style="width: 100%;">
+                </div>
+                <div class="card-body">
+                    <p class="m-0 text-justify"> converse 70s </p>
+                </div>
+                <div class="card-footer d-flex flex-row justify-content-between align-items-center">
+                    <p class="m-0" style="font-size: 16px; font-weight:600;">Rp. 560.000</p>
+                    <button class="btn btn-outline-primary" style="font-size:24px">
+                        <i class="bi bi-bag-check-fill"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card" style="width:220px;">
+                <div class="card-header m-auto" style="border-radius:5px;">
+                    <img src="{{Vite::asset('resources/images/70s.jpg')}}" alt="sepatu" style="width: 100%;">
+                </div>
+                <div class="card-body">
+                    <p class="m-0 text-justify"> converse 70s </p>
+                </div>
+                <div class="card-footer d-flex flex-row justify-content-between align-items-center">
+                    <p class="m-0" style="font-size: 16px; font-weight:600;">Rp. 560.000</p>
+                    <button class="btn btn-outline-primary" style="font-size:24px">
+                        <i class="bi bi-bag-check-fill"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
